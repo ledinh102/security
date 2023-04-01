11e1f3071bf8ead8daf8fea5483a0ad7845b1e89
kay <lenguyennhutdinh>
2023-04-01 21:16:23 +0700

    security lv1

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
 //jshint esversion:6
 
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
 
 const app = express()
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.render('home')
+mongoose
+	.connect("mongodb://127.0.0.1:27017/usersDB")
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.log(err))
+
+const usersSchema = {
+	username: String,
+	password: String,
+}
+
+const User = mongoose.model("User", usersSchema)
+
+app.get("/", (req, res) => {
+	res.render("home")
 })
 
-app.get('/login', (req, res) => {
-    res.render('login')
+app.get("/login", (req, res) => {
+	res.render("login")
 })
 
-app.get('/register', (req, res) => {
-    res.render('register')
+app.get("/register", (req, res) => {
+	res.render("register")
 })
 
-app.listen(3000, (req, res) =>{
-    console.log('listening on port 3000')
+app.post("/register", (req, res) => {
+	const newUser = new User({
+		username: req.body.username,
+		password: req.body.password,
+	})
+	newUser
+		.save()
+		.then(() => res.render("secrets"))
+		.catch((error) => console.log(error))
+})
+
+app.post("/login", (req, res) => {
+	const username = req.body.username
+	const password = req.body.password
+
+    User.findOne({ username: username})
+        .then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets")
+                } else {
+                    console.log("Passwords do not match")
+                }
+            } else {
+                console.log(`Account not found`)
+            }
+        })
+
+})
+
+app.listen(3000, (req, res) => {
+	console.log("listening on port 3000")
 })
